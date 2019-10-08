d39986e60fab68a5225766bf521944df43f22e3e
Matt Maher <matt@maher-family.net>
2019-10-08 11:51:54 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# scu-issues
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const BaseContext = React.createContext();
+
+class Child extends React.Component {
+  static contextType = BaseContext;
+
+  shouldComponentUpdate() {
+    console.log("[Child] scu");
+    return false;
+  }
+  render() {
+    const { name } = this.context;
+    return <div>Hi there {name}</div>;
+  }
+}
+class Contexter extends React.Component {
+  state = {
+    name: "bob"
+  };
+  names = ["bob", "jim", "sally"];
+  handlePress = () => {
+    const nextName = this.names.shift();
+    this.names.push(nextName);
+    this.setState({ name: nextName });
+  };
+  render() {
+    return (
+      <BaseContext.Provider value={{ name: this.state.name }}>
+        <button onClick={this.handlePress}>Change Things</button>
+        <Child />
+      </BaseContext.Provider>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Contexter />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
